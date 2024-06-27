a3e57de446ddd37b49ed6a31fc82d7acc8ea258f
wdlut <j.krueger@weda.de>
2024-06-27 10:24:33 +0200

    show cd-rom as volume
    Signed-off-by: si458 <simonsmith5521@gmail.com>

--- agents/modules_meshcore/computer-identifiers.js
+++ agents/modules_meshcore/computer-identifiers.js
@@ -63,6 +63,17 @@ function dataHandler(c)
     this.str += c.toString();
 }
 
+function formatDate_yyymmdd_hhmmss(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return year+"-"+month+"-"+day+" "+hours+":"+minutes+":"+seconds;
+}
+
 function linux_identifiers()
 {
     var identifiers = {};
@@ -344,16 +355,17 @@ function linux_identifiers()
         { }
         child = null;
     }
-
     // Linux Last Boot Up Time
     try {
-        child = require('child_process').execFile('/usr/bin/uptime', ['', '-s']); // must include blank value at begining for some reason?
-        child.stdout.str = ''; child.stdout.on('data', function (c) { this.str += c.toString(); });
-        child.stderr.on('data', function () { });
-        child.waitExit();
-        values.linux.LastBootUpTime = child.stdout.str.trim();
-        child = null;
-    } catch (ex) { }
+        const data = require('fs').readFileSync('/proc/uptime').toString().trim();
+        const uptimeSeconds = parseFloat(data.split(' ')[0]);
+        values.linux.upTimeSeconds = uptimeSeconds;
+        const bootTime = new Date( Date.now() - uptimeSeconds * 1000 );
+        const formattedBootTime = formatDate_yyymmdd_hhmmss(bootTime);
+        values.linux.LastBootUpTime = formattedBootTime;
+    } catch (ex) {  
+        console.error("Failed to read and process /proc/uptime:", ex.message);
+    }
 
     // Linux TPM
     try {
